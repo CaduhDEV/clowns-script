ca305a5803d2f9631dc9233b01d4b45337748467
CaduhDEV <contato.caduh@hotmail.com>
2022-02-10 19:32:05 -0300

    initial commit for main branch.

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'CaduhDEV <contato.caduh@hotmail.com>'
+
+description 'Killer Clowns as contribution to fivem community.'
+version '1.0.0'
+
+
+client_scripts {
+    'client-side/*.lua'
+}
+
+server_scripts {
+    'server-side/*.lua'
+}
